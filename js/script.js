70f0b730633020eebd5e827cfa2eee696ca7e86e
cornfield <cornfield@cornfields-MacBook-Air.local>
2023-06-14 19:27:49 +0700

    updating

--- js/script.js
+++ js/script.js
@@ -89,27 +89,24 @@ $(document).ready(function () {
 
 })
 
-function countdown() {
-    const targetDate = new Date('May 28, 2023 11:00:00').getTime();
+function countDate () {
+    const targetDate = new Date('May 28, 2023 12:00:00').getTime();
     const now = new Date().getTime();
-    const difference = targetDate - now;
-  
-    // Calculate days, hours, minutes, and seconds
+    const difference = now - targetDate;
+    // const difference = targetDate - now;
+
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
-    // Update the display
+   
     document.getElementById('day').innerText = days;
     document.getElementById('hour').innerText = hours;
     document.getElementById('minute').innerText = minutes;
     document.getElementById('second').innerText = seconds;
-  }
+}
   
-  // Call the countdown function every second
-  setInterval(countdown, 1000);
- 
+setInterval(countDate, 1000);
 
 //   COPY
 function copyBanking() {
